61c4ee75497c854338efdd15974a4bbaf64836e2
Nícolas do Couto Chaves <ncolascoutochaves@gmail.com>
2022-04-13 15:25:34 -0300

    improved style and some texts in script for ex017
    Melhorei o estilo do exercicio 17 e também o script, para que o resultado fique mais fácil de entender

--- ex017/script.js
+++ ex017/script.js
@@ -5,14 +5,19 @@ function verificar(){
     var num1output = Number(num1input)
     var num2output = Number(num2input)
     var resultado = document.querySelector('div#resultado')
+    var diferença = ""
     if (num1output > num2output){
-        resultado.innerHTML = `O número <strong>${num1output}</strong> é <strong>MAIOR</strong> que o <strong>${num2output}</strong>`
+        diferença = num1output - num2output
+        resultado.innerHTML = `Calculamos que <strong>A</strong> é maior do que <strong>B</strong>.`
+        resultado.innerHTML += `<br> <br> <p>A <strong>diferença</strong> entre <strong>A</strong> e <strong>B</strong> é de <strong>${diferença}</strong></p>`
     }
     else if (num2output > num1output){
-        resultado.innerHTML = `O número <strong>${num1output}</strong> é <strong>MENOR</strong> que o <strong>${num2output}</strong>`
+        diferença = num2output - num1output
+        resultado.innerHTML = `Calculamos que <strong>B</strong> é maior do que <strong>A</strong>.`
+        resultado.innerHTML += `<br> <br> <p>A <strong>diferença</strong> entre <strong>B</strong> e <strong>A</strong> é de <strong>${diferença}</strong></p>`
     }
     else {
-        resultado.innerHTML = `O número <strong>${num1output}</strong> é <strong>IGUAL</strong> ao <strong>${num2output}</strong>`
+        resultado.innerHTML = `Calculamos que <strong>A</strong> e <strong>B</strong> são iguais!`
 
     }
 }
