2f08409851e8747b6ec3c14fa14e5255f5c82bff
Nícolas do Couto Chaves <ncolascoutochaves@gmail.com>
2022-05-28 13:17:59 -0300

    Tabuada redondinha
    Terminei a tabuada!

--- ex021/script.js
+++ ex021/script.js
@@ -1,49 +1,31 @@
-
-function calcular(){
-<<<<<<< Updated upstream
-
-    var multiplicando = (document.getElementById('numero').value)
-    var resultado = document.querySelector('div#resultado')
-    let limiter = document.getElementById('limiter').value
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    let limiter = document.getElementById('limiter')
     let limit = ""
-    if (limiter.length == 0){
-        limit = 10
-    }
-    else{
-        limit = Number(limiter)
-    }
-
-
-/* 
-    function showResult(){
-    resultado.innerHTML=""
-    for (let count=0; count < limit; count++)
-    {
-        let produto = multiplicando * count
-    resultado.innerHTML += `${multiplicando}x${count} = ${produto} <br>`
-    }
-} */
-=======
-    var multiplicando = Number(document.getElementById('numero').value)
-    var multiplicador = 0
-    var resultado = document.querySelector('div#resultado')
-    var produto = multiplicando * multiplicador
-    resultado.innerHTML = ""
-    do {
-        resultado.innerHTML += `${multiplicando} x ${multiplicador} = ${produto} <br>`
-        multiplicador++
-    } while (multiplicador <= 10)
->>>>>>> Stashed changes
-
-    if (multiplicando.length == 0){
-    alert("Verifique os dados e tente novamente!")
-    }
-    else {
-       /*  let produto = multiplicando * count
-        let item = document.createElement('option')
-        item.text = `${multiplicando}x${count}=${produto}`
-        item.value = `tab${count}`
-        resultado.appendChild('item')
-     */
+        if (limiter.value.length == 0 || Number(limiter.value) == 0) {
+            limit = 10
+        }
+        else if (Number(limiter.value) < 0) {
+            alert("[Aviso] Digite somente limites maiores que 0")
+            limit = Number(limiter.value) * -1
+        }
+        else {
+            limit = Number(limiter.value)
+        }
+    if (num.value.length == 0){
+        alert("Verifique os dados e tente novamente!")
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.size = limit
+        tab.innerHTML = ""
+        while (c <= limit ){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n * c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
     }
 }
