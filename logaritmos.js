be4b5414391f0aa600bd1d163b1231d5ac793d07
Nicolas Chaves <ncolascoutochaves@gmail.com>
2022-07-31 22:38:37 -0300

    created some files to improve JS code skills

---
+++ logaritmos.js
@@ -0,0 +1,29 @@
+//Calculadora de logaritmos
+
+
+var resto = []
+
+function numerosprimos(n){
+    if (n <= 0 || n == NaN || n == undefined){
+        return "Número inválido!"
+    }
+    else if(n == 2){
+        return "É primo!"
+    }
+    else {
+        for(var divisor = 2; divisor < n; divisor++){
+        resto.push(n % divisor)
+    }
+    if(resto.indexOf(0) != -1){
+        return "NÃO É primo!"
+    } else{
+        return "É primo!"
+    }
+}   
+}
+
+
+
+
+/* function logaritmos(a=10, b, x){
+} */
