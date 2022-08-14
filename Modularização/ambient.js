05c4d2687240b2eadd06244fc6d0f62b340240de
Nicolas Chaves <ncolascoutochaves@gmail.com>
2022-08-13 23:56:11 -0300

    created 'Modularização' folder

---
+++ Modularização/ambient.js
@@ -0,0 +1,8 @@
+import { amigo, numeros } from './module.js';
+
+
+const numeroTeste = 15
+
+console.log(amigo.nome)
+console.log(numeros)
+console.log(numeroTeste)
---
+++ Modularização/module.js
@@ -0,0 +1,4 @@
+let amigo = {nome:'João', idade:25};
+let numeros = [2,4,6,4,3];
+
+export { amigo, numeros }; 
