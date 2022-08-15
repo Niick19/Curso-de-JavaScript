e5d685d835cada9aabf7d650dda9fd83b732e877
Nícolas do Couto Chaves <100481997+nicolascoutochaves@users.noreply.github.com>
2022-08-15 15:37:07 -0300

    Created structural HTML model for new project

---
+++ medias-e-conceitos/config.js
@@ -0,0 +1,13 @@
+const turma = {
+    numeroturma:301,
+    alunosturma:[],
+    teste(){
+        console.log(turma)
+        console.log('funcionou!')
+}
+}
+const inputTurma = document.getElementById('numTurma')
+function selectClassgroup() {
+    const turmaAtual = Number(inputTurma.value)
+    
+}
