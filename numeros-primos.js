e9a6db03ade829ebf09d06348d777bd6cc1b94b2
Nícolas do Couto Chaves <100481997+nicolascoutochaves@users.noreply.github.com>
2023-03-07 20:32:35 -0300

    Prime numbers
    Tabela que calcula números primos na força bruta!

--- numeros-primos.js
+++ numeros-primos.js
@@ -2,25 +2,38 @@
 
 
 var resto = []
-
+let primos = []
+var inicial
+inicial = 2
 function numerosprimos(n){
+    
     if (n <= 0 || n == NaN || n == undefined){
         return "Número inválido!"
     }
     else if(n == 2){
+        primos.push(n)
+        inicial++
         return "É primo!"
     }
     else {
+        resto = []
         for(var divisor = 2; divisor < n; divisor++){
         resto.push(n % divisor)
     }
     if(resto.indexOf(0) != -1){
+        inicial++
+        resto = []
         return "NÃO É primo!"
     } else{
+        primos.push(n)
+        inicial++
         return "É primo!"
     }
-}
-        
+}  
+
 }
 
-console.log(numerosprimos(997))
+while (inicial <= 300){
+    console.log(inicial, numerosprimos(inicial))
+}
+console.log(primos)
